3722cab25e9c9145338c298737ef27764d917b5c
netflair <dev@netflair.at>
2018-06-30 23:12:48 +0200

    -

--- inc/scripts/filter.php
+++ inc/scripts/filter.php
@@ -14,29 +14,29 @@ if ($_POST['selected'] == "all"){
     $selected = "";    
 }
 
-        $statement = $db_con->query("SELECT * FROM cars 
-                                    JOIN locations ON locations.loc_id = cars.fk_location 
-                                     $selected");
-        foreach ($statement as $row){ ?>
-           
-           <div class="card">
-               <div class="img">
-                <img src=" <?php echo $row['image'] ?> ">
-                <h1><?php echo $row['type'] ?></h1>
-               </div>
-               <div class="details">
-                   <h1><?php echo $row['brand'] . " " . $row['model'] ?></h1>
-                   <span><i class="far fa-arrow-alt-circle-right"></i>SEATS: <?php echo $row['passengers'] ?></span>
-                   <span><i class="far fa-arrow-alt-circle-right"></i>DOORS: <?php echo $row['doors'] ?></span>
-                   <br>
-                   <span><i class="far fa-arrow-alt-circle-right"></i>TRANSMISSION: <?php echo $row['transmission'] ?></span>
-                   <br>
-                   <span><i class="fas fa-euro-sign"></i>per day: <h2><?php echo $row['price_day'] ?></h2></span><hr>
-                   <span class="rent">Rent now</span>
-               </div>
-           </div>
+    $statement = $db_con->query("SELECT * FROM cars 
+                                JOIN locations ON locations.loc_id = cars.fk_location $selected");
+    foreach ($statement as $row){ ?>
+        
+        <div class="card">
+            <div class="img">
+            <img src=" <?php echo $row['image'] ?> ">
+            <h1><?php echo $row['type'] ?></h1>
+            <small>current location:<br><?php echo $row['address'] ?></small>
+            </div>
+            <div class="details">
+                <h1><?php echo $row['brand'] . " " . $row['model'] ?></h1>
+                <span><i class="far fa-arrow-alt-circle-right"></i>SEATS: <?php echo $row['passengers'] ?></span>
+                <span><i class="far fa-arrow-alt-circle-right"></i>DOORS: <?php echo $row['doors'] ?></span>
+                <br>
+                <span><i class="far fa-arrow-alt-circle-right"></i>TRANSMISSION: <?php echo $row['transmission'] ?></span>
+                <br>
+                <span><i class="fas fa-euro-sign"></i>per day: <h2><?php echo $row['price_day'] ?></h2></span><hr>
+                <span class="rent">Rent now</span>
+            </div>
+        </div>
 
-       <?php }
+    <?php }
 
 ?>
 
